2d5d60b938c7a214144562a48f0d0ced4fcf1b91
Ignium12 <kevin.ruehl12@googlemail.com>
2021-02-02 16:16:44 +0100

    Added support for adding an employee and getting an employee by id

--- src/main/java/com/kruehl/springboot/demo/SpringBootWebApp/rest/EmployeeRestController.java
+++ src/main/java/com/kruehl/springboot/demo/SpringBootWebApp/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.kruehl.springboot.demo.SpringBootWebApp.rest;
 import com.kruehl.springboot.demo.SpringBootWebApp.entity.Employee;
 import com.kruehl.springboot.demo.SpringBootWebApp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +22,26 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("Emplyoee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+
+        employee.setId(0);
+
+        employeeService.save(employee);
+
+        return employee;
+    }
+
+
 }
